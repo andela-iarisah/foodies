e2e716892d6a1b1ae017fceddd5fa74968e782a0
Ijeoma <gift_ij2002@yahoo.com>
2014-09-18 20:38:37 +0100

    updated validation; not yet done

--- scripts/jsfiles.js
+++ scripts/jsfiles.js
@@ -12,39 +12,58 @@ var foodies = {
 		$('#submit').click(foodies.searchRecipe);
 	},
 
-	// animation: function() {
-	// 	$('#submit').click(function() {
-
-	// 	});
-	// },
-
-	iteration: function(response) {
+	iteration: function(data) {
 		$('div#toAttach').empty();
+		// foodies.parameter.requirePictures = false;
 		var image = '<ul>';
 		var recipe_id;
-		$.each(response.matches, function(index, food) {
+		$.each(data.matches, function(index, food) {
 				recipe_id = food.id;
 				console.log(recipe_id);
+				console.log("expect respoce");
+				
+
+				if (data.totalMatchCount == 0) {
+					alert('nothing');
+					$('div.header').hide();
+					$('div.toAttach').append(error).append('<p class= "right">Sorry your input is temporarily unavailable</p>').append(errorRedirect)
+				// foodies.iteration(response.matches);
+				}
+				else if ($.isEmptyObject(food.imageUrlsBySize)) {
+					image += '<a target= "_blank" href= "' + foodies.base_0 + "external/" + recipe_id + '"><li class= "style">';
+					image += '<img class= "resize" src="images/unavailable.jpg">';
+					image += '<p class= "ingredients hidden">' + recipe_id + '<br><br><input type= "button"  class="recipe" value="Get Recipe"></p>';
+				image +=  '<p class= "width">' + food.recipeName + '</p></li></a>';	
+				}
+				else {
 				image += '<a target= "_blank" href= "' + foodies.base_0 + "external/" + recipe_id + '"><li class= "style">';
 				image += '<img class= "resize" src="' + food.imageUrlsBySize[90] + '">';
 				image += '<p class= "ingredients hidden">' + recipe_id + '<br><br><input type= "button"  class="recipe" value="Get Recipe"></p>';
 				image +=  '<p class= "width">' + food.recipeName + '</p></li></a>';	
+			}
+
 			});
 		image += '</ul>'
 		$('div#toAttach').append(image);
 	},
 
-	searchRecipe: function() {
+	searchRecipe: function(response) {
+
+			
 		foodies.parameter.q = $("#search").val();
+		var error = '<div class= "error"><img height= "500px" width= "500px" class= "center" src= "images/error.jpg"/></div>';
+		var errorRedirect = '<div class= "align"><a href= "index.html"><input id= "return" type= "button" value= "Return to Page"></div></a>';
 		if (!isNaN(foodies.parameter.q)) {
 			$('div.header').hide();
-			$('div#toAttach').append('<div class= "error"><img height= "500px" width= "500px" class= "center" src= "images/error.jpg"/></div>').append('<p class= "right">Uh-oh!</p>').append('<div class= "recover"><a href= "index.html"><input id= "return" type= "button" value= "Return to Page"></a></div>');
+			$('div#toAttach').append(error).append('<p class= "right">Please type a valid input</p>').append(errorRedirect);
 		}
-		else{
+
+		
+		else {
 			console.log(foodies.parameter.q);
 			var search;
 				$.getJSON(foodies.base + 'api/recipes?callback=?', foodies.parameter, function(response){
-					console.log(response);
+						console.log(response);
 					foodies.iteration(response);
 			});
 				foodies.parameter.q = $('#search').val('');
