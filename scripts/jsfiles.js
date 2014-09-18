c3360fb0852bb7c0f5cd53099137b272a959c0ad
Ijeoma <gift_ij2002@yahoo.com>
2014-09-18 13:07:49 +0100

    updated styling

--- scripts/jsfiles.js
+++ scripts/jsfiles.js
@@ -1,47 +1,76 @@
 var foodies = {
-	base: "https://api.yummly.com/v1/api/",
+	base_0: "http://www.yummly.com/recipe/",
+	base: "http://api.yummly.com/v1/",
 	parameter: {
 		_app_key: "87f181456268c338b0edf7bdb5006a77",
 		_app_id: "5e4de867",
 		q: null,
+		recipe_id: null,
 	},
 	
 	init: function() {
 		$('#submit').click(foodies.searchRecipe);
-		$('li.style').click(foodies.getRecipe);
 	},
 
-	iteration: function(response) {
+	iterationSearch: function(response) {
 		$('div#toAttach').empty();
 		var image = '<ul>';
 		$.each(response.matches, function(index, food) {
-				image += '<li class= "style">';
-				image += '<a class= "float" href= "' + food.imageUrlsBySize[90] + '">';
+				// image += '<a href= "' + foodies.base + 'api/recipe/' + food.id + '?_app_id=' +'">';
+				image += '<a name= "recipe"><li class= "style">';
 				image += '<img class= "resize" src="' + food.imageUrlsBySize[90] + '">';
-				image +=  '<p class= "width">' + food.recipeName + '</p></li></a>';
+				// image += '<p class= "id">' + food.id + '</p>';
+				image += '<p class= "ingredients hidden">' + food.ingredients + '<br><br><input type= "submit" value="Get Recipe" class="recipe"></p>';
+				image +=  '<p class= "width">' + food.recipeName + '</p>';
+				image += '<input type= "submit" value="Click me!" class="button"></li></a>';
+				
 			});
 		image += '</ul>'
 		$('div#toAttach').append(image);
+		$('input.button').click(function () {
+			// $(this).attr('disabled', true);
+			console.log('Show me');
+			$('.button').siblings('p.ingredients').addClass('hidden');
+			$(this).siblings('p.ingredients').removeClass('hidden');
+		});
+			$('input.recipe').click(function(response) {
+				$.each(response.matches, function(index, recipe) {
+					foodies.parameter.recipe_id = recipe.recipe_id;
+				});
+				$('div.hide').html(foodies.base_0 + foodies.parameter.recipe_id);
+			});
 	},
+
+	// iterationGet: function(response) {
+	// 	var get = 
+	// 		$.each('#toAttach', function(index, recipe) {
+	// 			foodies.parameter.recipe_id = recipe.recipe_id;
+	// 		});
+	// 			$.getJSON(foodies.base + 'recipes?callback=?', foodies.parameter, function(recipe){
+	// 					$('li.style').click(function() {
+	// 						$(this).window.open(foodies.base + 'recipes?callback?');
+	// 				});
+	// 			});
+	// },
 
 	searchRecipe: function() {
 		foodies.parameter.q = $("#search").val();
 			console.log(foodies.parameter.q);
 			var search;
-				$.getJSON(foodies.base + 'recipes?callback=?', foodies.
+				$.getJSON(foodies.base + 'api/recipes?callback=?', foodies.
 				parameter, function(response){
 					console.log(response);
-					foodies.iteration(response);
+					foodies.iterationSearch(response);
+					for (var i = 0; i<11; i++) {
+						console.log(response.matches[i]);
+					}
 			});
 				foodies.parameter.q = $('#search').val('');
 	},	
 
-	getRecipe: function() {
-		foodies.iterationGet(recipe);
-			$.getJSON(foodies.base + 'recipe/recipe-id?callback=?', foodies.parameter + result, function(recipe){
-				console.log(recipe);
-			});
+	// getRecipe: function() {
 		
-	},
+	// 	foodies.iterationGet(response);
+	// },
 }
 $(document).ready(foodies.init);
