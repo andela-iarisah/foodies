8e859291397edbb265089a3b1e1f384fd1b321bb
Ijeoma <gift_ij2002@yahoo.com>
2014-09-19 09:52:59 +0100

    validation fixed

--- scripts/jsfiles.js
+++ scripts/jsfiles.js
@@ -1,3 +1,5 @@
+var error = '<div class= "error"><img height= "500px" width= "500px" 			class= "center" src= "images/error.jpg"/></div>';
+var errorRedirect = '<div class= "align"><a href= "index.html"><input 					id= "return" type= "button" value= "Return to Page">   				</div></a>';
 var foodies = {
 	base_0: "http://www.yummly.com/recipe/",
 	base: "http://api.yummly.com/v1/",
@@ -14,22 +16,12 @@ var foodies = {
 
 	iteration: function(data) {
 		$('div#toAttach').empty();
-		// foodies.parameter.requirePictures = false;
 		var image = '<ul>';
 		var recipe_id;
-		$.each(data.matches, function(index, food) {
+		if (data.matches.length > 0) {
+			$.each(data.matches, function(index, food) {
 				recipe_id = food.id;
-				console.log(recipe_id);
-				console.log("expect respoce");
-				
-
-				if (data.totalMatchCount == 0) {
-					alert('nothing');
-					$('div.header').hide();
-					$('div.toAttach').append(error).append('<p class= "right">Sorry your input is temporarily unavailable</p>').append(errorRedirect)
-				// foodies.iteration(response.matches);
-				}
-				else if ($.isEmptyObject(food.imageUrlsBySize)) {
+				 if ($.isEmptyObject(food.imageUrlsBySize)) {
 					image += '<a target= "_blank" href= "' + foodies.base_0 + "external/" + recipe_id + '"><li class= "style">';
 					image += '<img class= "resize" src="images/unavailable.jpg">';
 					image += '<p class= "ingredients hidden">' + recipe_id + '<br><br><input type= "button"  class="recipe" value="Get Recipe"></p>';
@@ -41,29 +33,25 @@ var foodies = {
 				image += '<p class= "ingredients hidden">' + recipe_id + '<br><br><input type= "button"  class="recipe" value="Get Recipe"></p>';
 				image +=  '<p class= "width">' + food.recipeName + '</p></li></a>';	
 			}
-
 			});
-		image += '</ul>'
-		$('div#toAttach').append(image);
+		}
+		else {
+			$('div.header').hide();
+			$('body').append(error).append('<p class= "right">Sorry your input is temporarily unavailable</p>').append(errorRedirect);
+		}
+			image += '</ul>'
+			$('div#toAttach').append(image);
 	},
 
 	searchRecipe: function(response) {
-
-			
-		foodies.parameter.q = $("#search").val();
-		var error = '<div class= "error"><img height= "500px" width= "500px" class= "center" src= "images/error.jpg"/></div>';
-		var errorRedirect = '<div class= "align"><a href= "index.html"><input id= "return" type= "button" value= "Return to Page"></div></a>';
+		foodies.parameter.q = $("#search").val();	
 		if (!isNaN(foodies.parameter.q)) {
 			$('div.header').hide();
 			$('div#toAttach').append(error).append('<p class= "right">Please type a valid input</p>').append(errorRedirect);
 		}
-
-		
 		else {
-			console.log(foodies.parameter.q);
 			var search;
 				$.getJSON(foodies.base + 'api/recipes?callback=?', foodies.parameter, function(response){
-						console.log(response);
 					foodies.iteration(response);
 			});
 				foodies.parameter.q = $('#search').val('');
